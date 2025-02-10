95135dec8405c7217b77f1b2556b52f0a730d90e
tenno570 <tennysonbrazier@gmail.com>
2025-02-10 17:45:06 +1100

    Still working on function 3

--- main.py
+++ main.py
@@ -12,16 +12,20 @@ def words():
         print (counts)
 
 def character_count():
+    counts = {}
+    alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ]
+
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        lower_contents = file_contents.lower
-        print (lower_contents)
-        alphabet =[alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-
-
-
-
-
+        lower_contents = file_contents.lower()
+        for x in lower_contents:
+            if x in counts :
+                counts[x] += 1
+            else :
+                counts[x] = 1
+            
+                
+    print (counts)
 
 
 character_count()
